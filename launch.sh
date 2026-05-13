226e6f7290014b7ae9d2e7f13f6d13b9df8d6e6a
johnstclair <167724101+johnstclair@users.noreply.github.com>
2026-05-13 15:47:53 -0700

    init

---
+++ launch.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+qemu-system-x86_64 \
+        -boot c \
+        -drive format=qcow2,file=Shrine.HDD \
+        -machine kernel_irqchip=off,pcspk-audiodev=audio0 \
+        -smp cores=4 \
+        -m 2048 \
+        -rtc base=localtime \
+        -audiodev pa,id=audio0 \
+	-serial null \
+	-serial null \
+        -serial tcp:127.0.0.1:7777,server,wait
